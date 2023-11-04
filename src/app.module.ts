9cdb3b69051676848aa2176fc00fa48ed5738203
carvalho-henrique <hccarvalho.henrique@gmail.com>
2023-11-04 18:52:11 -0300

    feat(auth) implement login, guard & local.strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { SuthService } from './suth/suth.service';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, SuthService],
+  providers: [AppService],
 })
 export class AppModule {}
