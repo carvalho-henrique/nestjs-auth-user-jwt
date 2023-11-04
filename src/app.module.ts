bb58ddc1470902385d3892f0f88b06a09eee8052
carvalho-henrique <hccarvalho.henrique@gmail.com>
2023-11-04 18:31:01 -0300

    chore(auth) create, module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { SuthService } from './suth/suth.service';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SuthService],
 })
 export class AppModule {}
