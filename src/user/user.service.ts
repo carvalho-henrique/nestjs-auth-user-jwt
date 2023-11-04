3a70f724395a7730ff0f890353c21c039fb4d932
carvalho-henrique <hccarvalho.henrique@gmail.com>
2023-11-04 18:27:45 -0300

    feat(user): implement findByEmail

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -22,4 +22,10 @@ export class UserService {
       password: undefined,
     };
   }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
 }
